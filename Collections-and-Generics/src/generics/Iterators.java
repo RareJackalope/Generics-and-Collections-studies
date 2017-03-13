8431aa2a156073367c6329d3363dbde56aeab89e
RareJackalope <rarejackalope@gmail.com>
2017-03-13 16:17:36 -0500

    Another static method using iterators to determine the dot product of two lists of doubles representing vectors.

--- Collections-and-Generics/src/generics/Iterators.java
+++ Collections-and-Generics/src/generics/Iterators.java
@@ -10,11 +10,13 @@ public class Iterators {
 	public static void main(String[] args) 
 	{
 		ArrayList<Double> doubles = new ArrayList<Double>(Arrays.asList(1.0,3.0,-2.0,-4.0,1.0,2.0));
+		ArrayList<Double> moreDoubles = new ArrayList<Double>(Arrays.asList(1.0,3.0,-2.0,-4.0,1.0,2.0));
 		
 		System.out.println("List before removing negatives " + doubles);
 		removeNegatives(doubles);
+		removeNegatives(moreDoubles);
 		System.out.println("List after removing negatives " + doubles);
-
+		System.out.println("Dot product of both lists equals: " + dot(doubles, moreDoubles));
 	}
 	
 	public static void removeNegatives(List<Double> v)
@@ -23,6 +25,23 @@ public class Iterators {
 		{
 			if (it.next() < 0) it.remove();
 		}
+	}
+	
+	public static double dot(List<Double> u , List<Double> v)
+	{	
+		if(u.size() != v.size())
+			throw new IllegalArgumentException();
+		double d = 0;
+		Iterator<Double> u_it = u.iterator();
+		Iterator<Double> v_it = v.iterator();
+		
+		while(u_it.hasNext())
+		{
+			assert v_it.hasNext();
+			d += u_it.next() * v_it.next();
+		}
+		
+		return d;
 	}
 
 }
