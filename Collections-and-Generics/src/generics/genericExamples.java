f9199fb87f5138f29dad4d9eb2d3594ef6f62616
RareJackalope <rarejackalope@gmail.com>
2017-03-13 15:36:55 -0500

    Simple example of using generics with a List collection.

--- Collections-and-Generics/src/generics/genericExamples.java
+++ Collections-and-Generics/src/generics/genericExamples.java
@@ -1,6 +1,18 @@
 package generics;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class genericExamples 
 {
-
+	public static void main(String[] args) 
+	{
+		List<Integer> ints = Arrays.asList(1,2,3);
+		int s = 0;
+		for(int n : ints)
+		{
+			s+=n;
+		}
+		System.out.println(s == 6);		
+	}
 }
