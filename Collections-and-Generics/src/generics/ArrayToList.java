04fd1ff6095fd069e360e70e91f8fb41235afd4f
RareJackalope <rarejackalope@gmail.com>
2017-03-13 17:45:59 -0500

    Using varargs to streamline the call to the asList generic method.

--- Collections-and-Generics/src/generics/ArrayToList.java
+++ Collections-and-Generics/src/generics/ArrayToList.java
@@ -8,17 +8,17 @@ public class ArrayToList
 {
 	public static void main(String[] args) 
 	{
-		List<Integer> ints = asList(new Integer[] {1,2,3});
-		List<String>  words = asList(new String[] { "hello","world" });
+		List<Integer> ints = asList(1,2,3);
+		List<String>  words = asList("hello","world" );
 		
 		System.out.println("List ints is an instance of Arrays? " + (ints instanceof Arrays));
 		System.out.println("List ints is an instance of List? " + (ints instanceof List));
 	}
 	
-	public static <E> List<E> asList (E[] arr)
+	public static <E> List<E> asList(E... arr)
 	{	
 		List<E> list = new ArrayList<E>();
-		for(E elm : arr) list.add(elm);
+		for(E elm: arr) list.add(elm);
 		return list;
 	}
 }
